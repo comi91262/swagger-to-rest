b72224f2c49d1dad7242ffbd6dd8b631f3b0749e
ykonomi <hgKaguyaToupako4er@gmail.com>
2022-05-18 01:52:13 +0900

    format

--- main.go
+++ main.go
@@ -23,59 +23,22 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(doc.BasePath)
+	host := "{{host}}"
+	basePath := doc.BasePath
 	for path, items := range doc.Paths {
-		fmt.Println(path)
-		for op, hoge := range items.Operations() {
-			fmt.Println(op)
-			for _, hoge := range hoge.Parameters {
-				fmt.Printf("debug: %v\n", hoge)
+		for method, op := range items.Operations() {
+			fmt.Printf("%s %s HTTP/1.1\n", method, host+basePath+path)
+			if len(op.Consumes) > 0 {
+				fmt.Printf("Content-Type: %s\n", op.Consumes[0])
 			}
-			//for resp, hoge := range hoge.Responses {
-			// fmt.Printf("debug: %v\n", resp)
-			// fmt.Printf("debug: %v\n", hoge)
-			//}
+			if op.Security != nil {
+				for _, auth := range *op.Security {
+					if _, ok := auth["api_key"]; ok {
+						fmt.Printf("Authorization: Bearer\n")
+					}
+				}
+			}
+			fmt.Println()
 		}
 	}
 }
-
-//swagger: "2.0"
-//info:
-//  title: Calculator Service
-//  description: HTTP service for multiplying numbers, a goa teaser
-//  version: ""
-//host: localhost:8088
-//consumes:
-//- application/json
-//- application/xml
-//- application/gob
-//produces:
-//- application/json
-//- application/xml
-//- application/gob
-//paths:
-//  /multiply/{a}/{b}:
-//    get:
-//      tags:
-//      - calc
-//      summary: multiply calc
-//      operationId: calc#multiply
-//      parameters:
-//      - name: a
-//        in: path
-//        description: Left operand
-//        required: true
-//        type: integer
-//      - name: b
-//        in: path
-//        description: Right operand
-//        required: true
-//        type: integer
-//      responses:
-//        "200":
-//          description: OK response.
-//          schema:
-//            type: integer
-//            format: int64
-//      schemes:
-//      - http
